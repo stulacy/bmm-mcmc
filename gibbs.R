29e4e32fa55b9b7c915082f23a446d9ce1273cb8
Stuart Lacy <stuart.lacy@gmail.com>
2019-07-09 11:44:25 +0100

    Initial commit. Simulated clean data

---
+++ gibbs.R
@@ -0,0 +1,3 @@
+# Runs Gibbs sampling
+
+df <- readRDS("data/N1000_P5_clean.rds")
---
+++ simulate_data.R
@@ -0,0 +1,20 @@
+# Firstly create a dataset with no noise, i.e. all variables have a different
+# theta for each cluster
+set.seed(17)
+N <- 1000
+P <- 5
+
+theta_actual <- matrix(c(0.7, 0.8, 0.2, 0.1, 0.1,
+                         0.3, 0.5, 0.9, 0.8, 0.6,
+                         0.1, 0.2, 0.5, 0.4, 0.9),
+                       nrow=3, ncol=5, byrow = T)
+cluster_ratios <- c(0.4, 0.3, 0.3)
+
+mat_list <- lapply(seq_along(cluster_ratios), function(i) {
+    n <- round(N * cluster_ratios[i])
+    sapply(theta_actual[i,], function(p) rbinom(n, 1, p))
+})
+
+mat <- do.call('rbind', mat_list)
+saveRDS(mat, "data/N1000_P5_clean.rds")
+
