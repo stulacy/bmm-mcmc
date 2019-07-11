ba3c82499cff257f7ce0d93847054c482f3bfd0e
stuart <stuart.lacy@gmail.com>
2019-07-11 01:14:27 +0100

    Added more datasets

--- simulate_data.R
+++ simulate_data.R
@@ -16,5 +16,39 @@ mat_list <- lapply(seq_along(cluster_ratios), function(i) {
 })
 
 mat <- do.call('rbind', mat_list)
-saveRDS(mat, "data/N1000_P5_clean.rds")
+saveRDS(mat, "data/K3_N1000_P5_clean.rds")
 
+
+set.seed(17)
+N <- 100
+P <- 5
+
+theta_actual <- matrix(c(0.7, 0.8, 0.2, 0.1, 0.1,
+                         0.2, 0.2, 0.9, 0.8, 0.6),
+                       nrow=2, ncol=5, byrow = T)
+cluster_ratios <- c(0.7, 0.3)
+
+mat_list <- lapply(seq_along(cluster_ratios), function(i) {
+    n <- round(N * cluster_ratios[i])
+    sapply(theta_actual[i,], function(p) rbinom(n, 1, p))
+})
+
+mat <- do.call('rbind', mat_list)
+saveRDS(mat, "data/K2_N100_P5_clean.rds")
+
+set.seed(17)
+N <- 1000
+P <- 5
+
+theta_actual <- matrix(c(0.7, 0.8, 0.2, 0.1, 0.1,
+                         0.2, 0.2, 0.9, 0.8, 0.6),
+                       nrow=2, ncol=5, byrow = T)
+cluster_ratios <- c(0.7, 0.3)
+
+mat_list <- lapply(seq_along(cluster_ratios), function(i) {
+    n <- round(N * cluster_ratios[i])
+    sapply(theta_actual[i,], function(p) rbinom(n, 1, p))
+})
+
+mat <- do.call('rbind', mat_list)
+saveRDS(mat, "data/K2_N1000_P5_clean.rds")
